e070f77335d90e1cd63f3005fd05058ecf7b41a3
Alyssa <atinsleylcpa@gmail.com>
2026-04-06 13:03:54 -0400

    Create affiliation_insert.sql

---
+++ fundraising/affiliation_insert.sql
@@ -0,0 +1,137 @@
+use impresario
+go
+
+declare 
+    @affiliation_no int,
+    @individual_customer_no int,
+    @group_customer_no int,
+    @affiliation_type_id int,
+    @is_allowed_to_transact int,
+    @is_included_in_search_results char(1),
+    @title varchar(50),
+    @salary money,
+    @note varchar(1024),
+    @start_dt datetime,
+    @end_dt datetime,
+    @inactive char(1),
+    @primary_ind char(1),
+    @name_ind int,
+    @affiliated_name varchar(55)
+
+declare taffiliation_cursor cursor for
+select 
+    individual_customer_no,
+    group_customer_no,
+    affiliation_type_id,
+    is_allowed_to_transact,
+    is_included_in_search_results,
+    title,
+    salary,
+    note,
+    start_dt,
+    end_dt,
+    inactive,
+    primary_ind,
+    name_ind,
+    affiliated_name
+from [INSERT NAME OF FLAT FILE HERE]
+
+open taffiliation_cursor
+
+fetch next from taffiliation_cursor into
+    @individual_customer_no,
+    @group_customer_no,
+    @affiliation_type_id,
+    @is_allowed_to_transact,
+    @is_included_in_search_results,
+    @title,
+    @salary,
+    @note,
+    @start_dt,
+    @end_dt,
+    @inactive,
+    @primary_ind,
+    @name_ind,
+    @affiliated_name
+
+while @@fetch_status = 0
+begin
+
+    if not exists (
+        select 1
+        from T_AFFILIATION
+        where individual_customer_no = @individual_customer_no
+          and group_customer_no = @group_customer_no
+          and inactive = 'N'
+    )
+    begin
+        exec @affiliation_no = ap_get_nextid_function 'RP', 1  
+
+        insert into T_AFFILIATION
+        (
+            affiliation_no,
+            individual_customer_no,
+            group_customer_no,
+            affiliation_type_id,
+            is_allowed_to_transact,
+            is_included_in_search_results,
+            title,
+            salary,
+            note,
+            start_dt,
+            end_dt,
+            inactive,
+            create_dt,
+            created_by,
+            last_update_dt,
+            last_updated_by,
+            primary_ind,
+            create_loc,
+            name_ind,
+            affiliated_name
+        )
+        values
+        (
+            @affiliation_no,
+            @individual_customer_no,
+            @group_customer_no,
+            @affiliation_type_id,
+            @is_allowed_to_transact,
+            @is_included_in_search_results,
+            @title,
+            @salary,
+            @note,
+            @start_dt,
+            @end_dt,
+            @inactive,
+            getdate(),
+            'conversion',
+            getdate(),
+            'conversion',
+            @primary_ind,
+            null,
+            @name_ind,
+            @affiliated_name
+        )
+    end
+
+    fetch next from taffiliation_cursor into
+        @individual_customer_no,
+        @group_customer_no,
+        @affiliation_type_id,
+        @is_allowed_to_transact,
+        @is_included_in_search_results,
+        @title,
+        @salary,
+        @note,
+        @start_dt,
+        @end_dt,
+        @inactive,
+        @primary_ind,
+        @name_ind,
+        @affiliated_name
+
+end
+
+close taffiliation_cursor
+deallocate taffiliation_cursor
