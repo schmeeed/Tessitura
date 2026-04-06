02657d147f108262f67a38738c84325302012ae6
Alyssa <atinsleylcpa@gmail.com>
2026-04-06 12:51:33 -0400

    Create cust_activity_insert.sql

---
+++ fundraising/cust_activity_insert.sql
@@ -0,0 +1,152 @@
+use impresario
+go
+
+declare 
+    @activity_no int,
+    @category varchar(50),
+    @activity_type int,
+    @customer_no int,
+    @contact_type int,
+    @inout_ind char(1),
+    @doc_no varchar(50),
+    @notes varchar(max),
+    @issue_dt datetime,
+    @urg_ind char(1),
+    @origin varchar(50),
+    @create_loc varchar(50),
+    @created_by varchar(50),
+    @create_dt datetime,
+    @last_updated_by varchar(50),
+    @last_update_dt datetime,
+    @perf_no int,
+    @pkg_no int,
+    @closed_ind char(1)
+
+declare tactivity_cursor cursor for
+select 
+    activity_no,
+    category,
+    activity_type,
+    customer_no,
+    contact_type,
+    inout_ind,
+    doc_no,
+    notes,
+    issue_dt,
+    urg_ind,
+    origin,
+    create_loc,
+    created_by,
+    create_dt,
+    last_updated_by,
+    last_update_dt,
+    perf_no,
+    pkg_no,
+    closed_ind
+from [INSERT NAME OF FLAT FILE HERE]
+
+open tactivity_cursor
+
+fetch next from tactivity_cursor into
+    @activity_no,
+    @category,
+    @activity_type,
+    @customer_no,
+    @contact_type,
+    @inout_ind,
+    @doc_no,
+    @notes,
+    @issue_dt,
+    @urg_ind,
+    @origin,
+    @create_loc,
+    @created_by,
+    @create_dt,
+    @last_updated_by,
+    @last_update_dt,
+    @perf_no,
+    @pkg_no,
+    @closed_ind
+
+while @@fetch_status = 0
+begin
+
+    -- If activity_no should be system-generated, uncomment below and remove from insert
+    -- exec @activity_no = ap_get_nextid_function 'AC', 1  
+
+    insert into T_CUST_ACTIVITY
+    (
+        activity_no,
+        category,
+        activity_type,
+        customer_no,
+        contact_type,
+        inout_ind,
+        doc_no,
+        notes,
+        issue_dt,
+        urg_ind,
+        origin,
+        create_loc,
+        created_by,
+        create_dt,
+        last_updated_by,
+        last_update_dt,
+        perf_no,
+        pkg_no,
+        closed_ind
+    )
+    values
+    (
+        @activity_no,
+        @category,
+        @activity_type,
+        @customer_no,
+        @contact_type,
+        @inout_ind,
+        @doc_no,
+        @notes,
+        @issue_dt,
+        @urg_ind,
+        @origin,
+        @create_loc,
+        @created_by,
+        @create_dt,
+        @last_updated_by,
+        @last_update_dt,
+        @perf_no,
+        @pkg_no,
+        @closed_ind
+    )
+
+    fetch next from tactivity_cursor into
+        @activity_no,
+        @category,
+        @activity_type,
+        @customer_no,
+        @contact_type,
+        @inout_ind,
+        @doc_no,
+        @notes,
+        @issue_dt,
+        @urg_ind,
+        @origin,
+        @create_loc,
+        @created_by,
+        @create_dt,
+        @last_updated_by,
+        @last_update_dt,
+        @perf_no,
+        @pkg_no,
+        @closed_ind
+
+end
+
+close tactivity_cursor
+deallocate tactivity_cursor
+
+
+-- to double check
+select top 10 *
+from T_CUST_ACTIVITY
+order by create_dt desc;
