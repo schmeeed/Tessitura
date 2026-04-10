79e135d872f831c26f792d15f7c8c9b24a7aa0ab
Alyssa <atinsleylcpa@gmail.com>
2026-04-10 15:53:35 -0400

    Update t_stepinsert.sql

--- fundraising/t_stepinsert.sql
+++ fundraising/t_stepinsert.sql
@@ -2,118 +2,66 @@ use impresario
 go
 
 declare 
+    @step_no int,
+    @plan_no int,
+    @step_dt datetime,
+    @step_type varchar(10),
+    @description varchar(255),
+    @notes varchar(255),
+    @associate_no int,
+    @due_dt datetime,
+    @completed_on_dt datetime,
+    @warning_days int,
+    @priority int,
+    @old_value varchar(255),
+    @new_value varchar(255),
+    @worker_customer_no int,
     @activity_no int,
-	@category varchar(50),
-    @activity_type int,
     @customer_no int,
-    @contact_type int,
-    @inout_ind char(1),
-    @doc_no varchar(50),
-    @notes varchar(max),
-    @issue_dt datetime,
-    @urg_ind char(1),
-    @origin varchar(50),
-    @perf_no int,
-    @pkg_no int,
-    @closed_ind char(1)
+    @cyclical_ind char(1)
 
-declare tactivity_cursor cursor for
+declare tstep_cursor cursor for
 select 
-    category,
-    activity_type,
-    customer_no,
-    contact_type,
-    inout_ind,
-    doc_no,
-    notes,
-    issue_dt,
-    urg_ind,
-    origin,
-    perf_no,
-    pkg_no,
-    closed_ind
-from [FILE NAME HERE]
+    plan_no, step_dt, step_type, description, notes, associate_no, due_dt, completed_on_dt,
+    warning_days, priority, old_value, new_value, worker_customer_no, activity_no, customer_no, cyclical_ind
+from [INSERT NAME OF FLAT FILE HERE]
 
-open tactivity_cursor
+open tstep_cursor
 
-fetch next from tactivity_cursor into
-    @category,
-    @activity_type,
-    @customer_no,
-    @contact_type,
-    @inout_ind,
-    @doc_no,
-    @notes,
-    @issue_dt,
-    @urg_ind,
-    @origin,
-    @perf_no,
-    @pkg_no,
-    @closed_ind
+fetch next from tstep_cursor into
+    @plan_no, @step_dt, @step_type, @description, @notes, @associate_no, @due_dt, @completed_on_dt,
+    @warning_days, @priority, @old_value, @new_value, @worker_customer_no, @activity_no, @customer_no, @cyclical_ind
 
 while @@fetch_status = 0
 begin
 
-    -- If activity_no should be system-generated, uncomment below
-    exec @activity_no = ap_get_nextid_function 'AC', 1  
+    -- Get next step_no for T_STEP
+    exec @step_no = ap_get_nextid_function 'SB', 1  
 
-    insert into T_CUST_ACTIVITY
+    insert into T_STEP
     (
-        activity_no,
-		category,
-        activity_type,
-        customer_no,
-        contact_type,
-        inout_ind,
-        doc_no,
-        notes,
-        issue_dt,
-        urg_ind,
-        origin,
-        perf_no,
-        pkg_no,
-        closed_ind
+        step_no,
+        plan_no, step_dt, step_type, description, notes, associate_no, due_dt, completed_on_dt,
+        warning_days, priority, old_value, new_value, worker_customer_no, activity_no, customer_no, cyclical_ind
     )
     values
     (
-        @activity_no,
-		@category,
-        @activity_type,
-        @customer_no,
-        @contact_type,
-        @inout_ind,
-        @doc_no,
-        @notes,
-        @issue_dt,
-        @urg_ind,
-        @origin,
-        @perf_no,
-        @pkg_no,
-        @closed_ind
+        @step_no,
+        @plan_no, @step_dt, @step_type, @description, @notes, @associate_no, @due_dt, @completed_on_dt,
+        @warning_days, @priority, @old_value, @new_value, @worker_customer_no, @activity_no, @customer_no, @cyclical_ind
     )
 
-    fetch next from tactivity_cursor into
-        @category,
-        @activity_type,
-        @customer_no,
-        @contact_type,
-        @inout_ind,
-        @doc_no,
-        @notes,
-        @issue_dt,
-        @urg_ind,
-        @origin,
-        @perf_no,
-        @pkg_no,
-        @closed_ind
+    fetch next from tstep_cursor into
+        @plan_no, @step_dt, @step_type, @description, @notes, @associate_no, @due_dt, @completed_on_dt,
+        @warning_days, @priority, @old_value, @new_value, @worker_customer_no, @activity_no, @customer_no, @cyclical_ind
 
 end
 
-close tactivity_cursor
-deallocate tactivity_cursor
+close tstep_cursor
+deallocate tstep_cursor
 
 
--- to double check
-select top 10 *
-from T_CUST_ACTIVITY
-order by activity_no desc;
+    --to double check
+select top 10*
+from t_step
+order by create_dt desc;
